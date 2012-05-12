fd182e904de6a5f28d6568e11d2cbb4954738dec
Can Tonic <can@cantonic.com>
2012-05-12 23:54:01 +0300

    application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piratenportal::Application.config.secret_token = 'b9c6ee4949dba6b421026f30592c0073c7fdb65049c4e805e6b73deef35ba16583d50df01a802ba5a0d1d9470c0189cb2e4d8ee34a60706f47e6543e7392d5e9'
